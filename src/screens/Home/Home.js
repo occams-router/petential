2950736809987d0a2a8b9d11929f48c1df941613
Jordan Walters <jwalters012@gmail.com>
2021-11-24 16:27:29 -0500

    Feat: Home Screen

---
+++ src/screens/Home/Home.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { View, Text, Button, Image } from "react-native";
+
+const Home = ({ navigation }) => {
+  return (
+    <View>
+      <Text>Are you a shelter or an adopter?</Text>
+      <View>
+        <Image
+          source={{
+            uri: "https://tinyurl.com/2w8xx583",
+          }}
+        />
+        <Button
+          title="I represent a shelter!"
+          onPress={() => navigation.navigate("Screen")}
+        />
+        <Image
+          source={{
+            uri: "http://www.aspca.org/sites/default/files/nyc-adoption-center-facebook.jpg",
+          }}
+        />
+        <Button
+          title="I want to adopt!"
+          onPress={() => navigation.navigate("Screen")}
+        />
+      </View>
+    </View>
+  );
+};
+
+export default Home;
