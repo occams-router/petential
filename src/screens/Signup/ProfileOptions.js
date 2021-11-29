fff56e3aaafbfc20121e0109a4589c2092b49a88
Anshu Patel <anshupatel314@gmail.com>
2021-11-29 11:23:56 -0500

    updated file structure

---
+++ src/screens/Signup/ProfileOptions.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { View, Text, Button, Image } from 'react-native';
+
+const ProfileOptions = ({ navigation }) => {
+  return (
+    <View>
+      <Text>Are you a shelter or an adopter?</Text>
+      <View>
+        <Image
+          source={{
+            uri: 'https://tinyurl.com/2w8xx583',
+          }}
+        />
+        <Button
+          title="I represent a shelter!"
+          onPress={() => navigation.navigate('ShelterSignup')}
+        />
+        <Image
+          source={{
+            uri: 'http://www.aspca.org/sites/default/files/nyc-adoption-center-facebook.jpg',
+          }}
+        />
+        <Button
+          title="I want to adopt!"
+          onPress={() => navigation.navigate('AdopterSignup')}
+        />
+      </View>
+    </View>
+  );
+};
+
+export default ProfileOptions;
--- src/screens/index.js
+++ src/screens/index.js
@@ -2,10 +2,8 @@ export { default as Login } from './Login/Login';
 
 export { default as FillerHome } from './Home/FillerHome';
 
-export { default as Signup } from './Signup/Signup';
+export { default as ProfileOptions } from './Signup/ProfileOptions';
 
 export { default as ShelterSignup } from './Signup/ShelterSignup';
 
 export { default as AdopterSignup } from './Signup/AdopterSignup';
-
-export { default as Home } from './Home/Home';
