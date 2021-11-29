6efe95d13f307567e1eea6367ec8199b0ce3d1a3
Anshu Patel <anshupatel314@gmail.com>
2021-11-29 12:35:09 -0500

    added home screens to index.js

--- src/screens/Home/ShelterHome.js
+++ src/screens/Home/ShelterHome.js
@@ -1,22 +1,22 @@
-import React from "react";
-import { Text, View, TouchableOpacity } from "react-native";
-import { auth } from "../../firebase/config";
-import styles from "./styles";
-import { signOut } from "@firebase/auth";
+import React from 'react';
+import { Text, View, TouchableOpacity } from 'react-native';
+import { auth } from '../../firebase/config';
+import styles from './styles';
+import { signOut } from '@firebase/auth';
 
-export default function FillerHome(props) {
+export default function ShelterHome(props) {
   const logout = async () => {
     try {
       await signOut(auth);
-      alert("You are logged out.");
+      alert('You are logged out.');
     } catch (error) {
-      alert("Log-out was unsuccesful.");
+      alert('Log-out was unsuccesful.');
       console.log(error.message);
     }
   };
   return (
     <View>
-      <Text style={styles.title}>Shelter Home Screen</Text>
+      <Text style={styles.title}>Shelter Feed</Text>
       <TouchableOpacity style={styles.button} onPress={() => logout()}>
         <Text style={styles.buttonTitle}>Log out</Text>
       </TouchableOpacity>
