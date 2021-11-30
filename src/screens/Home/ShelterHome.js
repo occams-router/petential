e460f0e0080d5f4eb74db35d5d6fef3b05b4f09f
Jordan Walters <jwalters012@gmail.com>
2021-11-30 18:31:26 -0500

    Refactor: Shelter Home updated w/ hooks but needs debug for user data.

--- src/screens/Home/ShelterHome.js
+++ src/screens/Home/ShelterHome.js
@@ -12,7 +12,7 @@ import { auth, db } from "../../firebase/config";
 import styles from "./styles";
 import { signOut } from "@firebase/auth";
 import { render } from "react-dom";
-import { snapshotEqual } from "firebase/firestore";
+import { snapshotEqual, doc } from "firebase/firestore";
 // import { useEffect } from "react/cjs/react.development";
 import PetCard from "../PetCards/ShelterPetCard";
 
@@ -61,8 +61,8 @@ const pets = [
 
 export default function ShelterHome(props) {
   const [petData, setPetData] = useState([]);
-  // const user = props.route.params.user;
-  // const id = user.id;
+  const user = props.route.params.user;
+  const id = user.id;
   const logout = async () => {
     try {
       await signOut(auth);
@@ -75,13 +75,14 @@ export default function ShelterHome(props) {
   const getPets = async () => {
     try {
       const list = [];
-      // const ShelterRef = doc(db, "shelters", id);
+      const ShelterRef = doc(db, "shelters", id);
+      console.log(ShelterRef);
       //for each pet in shelterpets collection push it into petData state
-      shelterRef.forEach((doc) => {
-        console.log(ShelterRef);
-        list.push(doc.data());
-      });
-      setPetData([...list]);
+      // shelterRef.forEach((doc) => {
+      //   console.log(ShelterRef);
+      //   list.push(doc.data());
+      // });
+      // setPetData([...list]);
     } catch (e) {
       console.error(e, "No Pets in DB");
     }
@@ -91,7 +92,7 @@ export default function ShelterHome(props) {
   useEffect(() => {
     getPets();
   }, []);
-
+  console.log(props);
   return (
     <SafeAreaView>
       <Text style={styles.title}>Current Pets</Text>
