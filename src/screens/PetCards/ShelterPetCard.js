cd1c26dfcf7fc216ff6eba3b891892d06fe50414
Jordan Walters <jwalters012@gmail.com>
2021-12-02 14:11:06 -0500

    style: update pet card for consistency'

--- src/screens/PetCards/ShelterPetCard.js
+++ src/screens/PetCards/ShelterPetCard.js
@@ -8,78 +8,92 @@ import {
   SafeAreaView,
   TouchableOpacity,
 } from "react-native";
+import styled from "styled-components/native";
+import { Card, Title, Paragraph, Button } from "react-native-paper";
+
+const Container = styled.View`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+`;
+
+const Header = styled.Text`
+  color: #000;
+  font-size: 26px;
+`;
+
+const CardContainer = styled.View`
+  width: 90%;
+  max-width: 260px;
+  height: auto;
+  margin: 20px;
+`;
+
+const xCard = styled.View`
+  position: relative;
+  background-color: #fff;
+  width: 100%;
+  max-width: 260px;
+  height: 300px;
+  shadow-color: black;
+  shadow-opacity: 0.2;
+  shadow-radius: 20px;
+  border-radius: 20px;
+  resize-mode: cover;
+`;
+
+const CardImage = styled.ImageBackground`
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+  border-radius: 20px;
+`;
+
+const CardTitle = styled.Text`
+  position: absolute;
+  bottom: 0;
+  margin: 10px;
+  color: #fff;
+`;
+
+const InfoText = styled.Text`
+  height: 28px;
+  justify-content: center;
+  display: flex;
+  z-index: -100;
+`;
+
+const ProfileInfo = styled.View`
+  margin-top: 10px;
+  display: flex;
+  flex-direction: column;
+  background-color: #fff;
+  border-radius: 20px;
+`;
+
+const NameAndAge = styled.Text`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+`;
 
 export default function PetCard({ pets }) {
   return (
-    <SafeAreaView>
-      <View style={styles.container}>
-        <View style={styles.cardContainer}>
-          <Image
-            style={styles.imageStyle}
-            source={{
-              uri: pets.imageUrl,
-            }}
-          />
-          <View style={styles.infoStyle}>
-            <Text style={styles.titleStyle}>{pets.name}</Text>
-            <Text style={styles.categoryStyle}>Species: {pets.breed}</Text>
-            <Text style={styles.categoryStyle}>Age: {pets.age}</Text>
-            <TouchableOpacity>
-              <Text>Edit Pet</Text>
-            </TouchableOpacity>
-          </View>
-        </View>
-      </View>
-    </SafeAreaView>
+    <Container>
+      <CardContainer>
+        <Card>
+          <Card.Cover source={{ uri: pets.imageUrl }}></Card.Cover>
+          <Title>{pets.name}</Title>
+          <Text>Breed: {pets.breed}</Text>
+          <Text>Age: {pets.age}</Text>
+          <Card.Actions>
+            <Button mode="contained" style={{ backgroundColor: "#788eec" }}>
+              Edit Pet
+            </Button>
+          </Card.Actions>
+        </Card>
+      </CardContainer>
+    </Container>
   );
 }
-
-const deviceWidth = Math.round(Dimensions.get("window").width);
-const offset = 40;
-const radius = 20;
-const styles = StyleSheet.create({
-  container: {
-    width: deviceWidth - 20,
-    alignItems: "center",
-    marginTop: 25,
-  },
-  cardContainer: {
-    width: deviceWidth - offset,
-    backgroundColor: "#788eec",
-    height: 220,
-    borderRadius: radius,
-
-    shadowColor: "#000",
-    shadowOffset: {
-      width: 5,
-      height: 5,
-    },
-    shadowOpacity: 0.75,
-    shadowRadius: 5,
-    elevation: 9,
-  },
-  imageStyle: {
-    height: 130,
-    width: deviceWidth - offset,
-    borderTopLeftRadius: radius,
-    borderTopRightRadius: radius,
-    opacity: 0.9,
-    alignContent: "center",
-    alignSelf: "center",
-  },
-  titleStyle: {
-    fontSize: 20,
-    fontWeight: "800",
-  },
-  categoryStyle: {
-    fontWeight: "200",
-  },
-  infoStyle: {
-    marginHorizontal: 10,
-    marginVertical: 5,
-  },
-  iconLabelStyle: {
-    flexDirection: "row",
-    marginTop: 10,
-  },
-});
