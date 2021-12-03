9e21fd7bd7add2624244bbbb61dde92abab1e49f
Anshu Patel <anshupatel314@gmail.com>
2021-12-03 11:45:12 -0500

    added matches to shelter sidebar

--- src/screens/Sidebar/ShelterSidebar.js
+++ src/screens/Sidebar/ShelterSidebar.js
@@ -1,7 +1,13 @@
 import React from 'react';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import { NavigationContainer } from '@react-navigation/native';
-import { ShelterHome, ShelterProfile, Header } from '../index.js';
+import {
+  ShelterHome,
+  ShelterProfile,
+  ShelterRequests,
+  ShelterMatches,
+  Header,
+} from '../index.js';
 import GlobalStyles from '../../../GlobalStyles.js';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
@@ -10,39 +16,35 @@ const Drawer = createDrawerNavigator();
 export default function ShelterSidebar() {
   return (
     <SafeAreaView style={GlobalStyles.droidSafeArea}>
-    <Drawer.Navigator
-      drawerType="front"
-      initialRouteName="Home"
-      drawerContentOptions={{
-        activeTintColor: "#e91e63",
-        itemStyle: { marginVertical: 10 },
-      }}
-    >
-      <Drawer.Screen
-        name="Home"
-        options={{ headerShown: true, header: () => <Header /> }}
-        component={ShelterHome}
-      />
-      <Drawer.Screen
-        name="Profile"
-        options={{ headerShown: true, header: () => <Header /> }}
-        component={ShelterProfile}
-      />
-      {/* <Drawer.Screen
-        name="Requests"
-        options={{ headerShown: true, header: () => <Header /> }}
-        component={ShelterRequests}
-      /> */}
-
-      {/* <Drawer.Screen
-        name="Requests"
-        options={{ headerShown: true, header: <Header /> }}
-      />
-      {<Drawer.Screen
-        name="Matches"
-        options={{ headerShown: true, header: <Header /> }}
-      /> */}
-    </Drawer.Navigator>
+      <Drawer.Navigator
+        drawerType="front"
+        initialRouteName="Home"
+        drawerContentOptions={{
+          activeTintColor: '#e91e63',
+          itemStyle: { marginVertical: 10 },
+        }}
+      >
+        <Drawer.Screen
+          name="Home"
+          options={{ headerShown: true, header: () => <Header /> }}
+          component={ShelterHome}
+        />
+        <Drawer.Screen
+          name="Profile"
+          options={{ headerShown: true, header: () => <Header /> }}
+          component={ShelterProfile}
+        />
+        <Drawer.Screen
+          name="Requests"
+          options={{ headerShown: true, header: () => <Header /> }}
+          component={ShelterRequests}
+        />
+        <Drawer.Screen
+          name="Matches"
+          options={{ headerShown: true, header: () => <Header /> }}
+          component={ShelterMatches}
+        />
+      </Drawer.Navigator>
     </SafeAreaView>
   );
 }
