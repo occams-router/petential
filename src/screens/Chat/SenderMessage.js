3310ae61c8af0d1a97d7619d98346ab99a9fce6d
Daryl Ramirez <darylc.ramirez@gmail.com>
2021-12-05 11:00:32 -0500

    (feat): timestamp on messages

--- src/screens/Chat/SenderMessage.js
+++ src/screens/Chat/SenderMessage.js
@@ -1,14 +1,22 @@
 import React from "react";
 import {View, Text} from 'react-native';
 import tailwind from "tailwind-rn";
+import Moment from 'react-moment'
 
 export default function SenderMessage({message}) {
     return (
+        <View>
         <View style={[tailwind('bg-blue-300 rounded-lg rounded-tr-none px-5 py-3 mx-3 my-2'), 
         {alignSelf: "flex-start", marginLeft: 'auto'},]}>
             <Text style={tailwind('text-white')}>
                 {message.message}
             </Text>
         </View>
+        <View style={[tailwind('px-5 py-3'), {alignSelf: "flex-start", marginLeft: 'auto'}]}>
+        <Text>
+<Moment fromNow>{message.timestamp.toDate()}</Moment>
+        </Text>
+    </View>
+    </View>
     )
 }
