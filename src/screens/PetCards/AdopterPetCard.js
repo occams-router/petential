02fa0095c4d453d860af6f2b1a0c393c4e077550
courtney <rebocourtney@gmail.com>
2021-12-05 13:10:44 -0800

    fix: buttons on pet swipe card are clickable

--- src/screens/PetCards/AdopterPetCard.js
+++ src/screens/PetCards/AdopterPetCard.js
@@ -1,8 +1,8 @@
-import React, { useEffect, useState, useContext } from 'react';
-import styled from 'styled-components/native';
-import { collection, getDocs, addDoc } from 'firebase/firestore';
-import { db } from '../../firebase/config';
-import TinderCard from '../../react-tinder-card/reactTinderCard';
+import React, { useEffect, useState, useContext } from "react";
+import styled from "styled-components/native";
+import { collection, getDocs, addDoc } from "firebase/firestore";
+import { db } from "../../firebase/config";
+import TinderCard from "../../react-tinder-card/reactTinderCard";
 import {
   Card,
   Title,
@@ -10,8 +10,8 @@ import {
   Button,
   Divider,
   Subheading,
-} from 'react-native-paper';
-import { UserContext } from '../../../App';
+} from "react-native-paper";
+import { UserContext } from "../../../App";
 
 const Container = styled.View`
   display: flex;
@@ -34,18 +34,25 @@ const InfoText = styled.Text`
   text-align: center;
 `;
 
+const ButtonContainer = styled.View`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  width: 100%;
+`;
+
 export default function AdopterPetCard(props) {
   const [lastDirection, setLastDirection] = useState();
   const [petsList, setPetsList] = useState([]);
   const user = useContext(UserContext);
 
-  const petsCollectionRef = collection(db, 'pets');
+  const petsCollectionRef = collection(db, "pets");
 
   const swiped = async (direction, pet) => {
-    console.log('removing:', pet.name);
+    console.log("removing:", pet.name);
 
     // check if the pet already exists in the user's 'seen' subcollection
-    const seenSubRef = collection(db, 'adopters', `${user.id}`, 'seen');
+    const seenSubRef = collection(db, "adopters", `${user.id}`, "seen");
     const seenPetsDocs = await getDocs(seenSubRef);
     const seenPetsArr = seenPetsDocs.docs.map((doc) => ({ ...doc.data() }));
     const petExists = seenPetsArr.find((element) => element.id === pet.id);
@@ -56,19 +63,19 @@ export default function AdopterPetCard(props) {
     }
 
     // if right swipe, add pet to its shelter's 'requests' subcollection
-    if (direction === 'right') {
+    if (direction === "right") {
       const requestsSubRef = collection(
         db,
-        'shelters',
+        "shelters",
         `${pet.shelterRefId}`,
-        'requests'
+        "requests"
       );
 
       const requestData = {
         petRefId: pet.id,
         adopterRefId: user.id,
         shelterRefId: pet.shelterRefId,
-        status: 'pending',
+        status: "pending",
       };
 
       await addDoc(requestsSubRef, requestData);
@@ -79,7 +86,7 @@ export default function AdopterPetCard(props) {
   };
 
   const outOfFrame = (name) => {
-    console.log(name + ' left the screen!');
+    console.log(name + " left the screen!");
   };
 
   useEffect(async () => {
@@ -106,14 +113,14 @@ export default function AdopterPetCard(props) {
                 key={pet.id}
                 onSwipe={(dir) => swiped(dir, pet)}
                 onCardLeftScreen={() => outOfFrame(pet.name)}
-                preventSwipe={['up', 'down']}
+                preventSwipe={["up", "down"]}
               >
                 <Card>
                   <Card.Cover source={{ uri: pet.imageUrl }}></Card.Cover>
 
                   <Card.Content>
                     <Title>
-                      {pet.name} ({pet.age} {pet.age > 1 ? 'years' : 'year'}{' '}
+                      {pet.name} ({pet.age} {pet.age > 1 ? "years" : "year"}{" "}
                       old)
                     </Title>
                     <Divider />
@@ -131,13 +138,20 @@ export default function AdopterPetCard(props) {
                   </Card.Content>
 
                   <Card.Actions>
-                    <Button icon="thumb-down-outline">Pass</Button>
-                    <Button
-                      icon="heart"
-                      onPress={() => console.log('You choose to pass.')}
-                    >
-                      Like
-                    </Button>
+                    <ButtonContainer>
+                      <Button
+                        icon="thumb-down-outline"
+                        onPress={() => console.log("You choose to pass.")}
+                      >
+                        Pass
+                      </Button>
+                      <Button
+                        icon="heart"
+                        onPress={() => console.log("You choose to like.")}
+                      >
+                        Like
+                      </Button>
+                    </ButtonContainer>
                   </Card.Actions>
                 </Card>
               </TinderCard>
