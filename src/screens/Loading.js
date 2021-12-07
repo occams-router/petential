ab93cdcdc08b34f540037226b55227d501178397
Anshu Patel <anshupatel314@gmail.com>
2021-12-07 16:56:39 -0500

    updated Loading import in home screens

--- src/screens/Loading.js
+++ src/screens/Loading.js
@@ -1,23 +1,27 @@
-import React from "react";
-import { ActivityIndicator, StyleSheet, Text, SafeAreaView } from "react-native";
+import React from 'react';
+import {
+  ActivityIndicator,
+  StyleSheet,
+  Text,
+  SafeAreaView,
+} from 'react-native';
 
-export default function Loading () {
-    return(
-  <SafeAreaView style={[styles.container, styles.horizontal]}>
-    <ActivityIndicator size="large" color="#FFC0CB" />
-  </SafeAreaView>
-    )
-};
+export default function Loading() {
+  return (
+    <SafeAreaView style={[styles.container, styles.horizontal]}>
+      <ActivityIndicator size="large" color="#FFC0CB" />
+    </SafeAreaView>
+  );
+}
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center"
+    justifyContent: 'center',
   },
   horizontal: {
-    flexDirection: "row",
-    justifyContent: "space-around",
-    padding: 10
-  }
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    padding: 10,
+  },
 });
-
