1b5c6460ce10543c9f6884452aa1ee769f1f085a
Daryl Ramirez <darylc.ramirez@gmail.com>
2021-12-08 11:17:57 -0500

    (feat): date picker front end

--- src/screens/Scheduling/Scheduler.js
+++ src/screens/Scheduling/Scheduler.js
@@ -50,6 +50,7 @@ const Scheduler = () => {
       </View>
       {show && (
         <DateTimePicker
+        minimumDate={new Date()}
           testID="dateTimePicker"
           value={date}
           mode={mode}
